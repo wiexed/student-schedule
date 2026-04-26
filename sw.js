d1e1c4fc450642fc8e4c3604613fe930df06a510
wiexed <kasperden67@gmail.com>
2026-04-27 00:37:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "schedule-v127";
+const CACHE_NAME = "schedule-v149";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
